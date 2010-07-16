c68c04bc916cfb823f266b197dfadae7c1be4841
aulitin <aulitin@5811a9ef-1b48-4737-9a9a-a0e711bb41ec>
2010-07-16 08:43:32 +0000

    fix dropdown menu

--- views/default/googleappslogin/login_dropdown.php
+++ views/default/googleappslogin/login_dropdown.php
@@ -1,5 +1,5 @@
 	<div id="login_dropdown" style="position: relative; top: 140px;">
-		<fieldset id="signin_menu" style="width: 215px;">
-			<a href="<?php echo $GLOBALS['googleappslogin_url'];?>"><img src="<?php echo $vars['url']; ?>mod/googleappslogin/graphics/login_with_google_apps.gif" alt="googleapps" /></a>
+		<fieldset id="signin_menu" style="">
+			<a href="<?php echo $GLOBALS['googleappslogin_url'];?>" style="width:215px;height:50px;float:left;text-align:center;"><img src="<?php echo $vars['url']; ?>mod/googleappslogin/graphics/login_with_google_apps.gif" alt="googleapps" /></a>
 		</fieldset>
 	</div>
