025c135d5926cd8ca253add67085f38fda24f888
Jeff Tilson <jrtilson@gmail.com>
2010-11-12 19:30:08 +0000

    Start.php: Code cleanup, fixed deprecated submenu functions. Updated to bring in line with: http://trac.elgg.org/changeset/7288 and to make the admin page work again

--- start.php
+++ start.php
@@ -10,7 +10,6 @@
  * @link http://elgg.org/
  */
 
-global $CONFIG;
 
 /**
  * googleappslogin initialisation
@@ -25,11 +24,14 @@ function googleappslogin_init() {
 
 	global $CONFIG;
 	
+	// Includes
 	require_once 'models/functions.php';
 	require_once 'models/admin_functions.php';
 	
+	// Need to use SSL for google urls
 	$CONFIG->sslroot = str_replace('http://','https://', $CONFIG->wwwroot);
 
+	// Set up some urls
 	$googleappslogin_url = elgg_add_action_tokens_to_url($CONFIG->sslroot . 'action/googleappslogin/login', FALSE);
 	$googleappsconnect_url = elgg_add_action_tokens_to_url($CONFIG->sslroot . 'action/googleappslogin/connect', FALSE);
 	$googleappsdisconnect_url = elgg_add_action_tokens_to_url($CONFIG->sslroot . 'action/googleappslogin/disconnect', FALSE);
@@ -37,6 +39,7 @@ function googleappslogin_init() {
 	$share_doc_url = elgg_add_action_tokens_to_url($CONFIG->sslroot . 'action/googleappslogin/share_doc', FALSE);
 	$change_doc_permissions_url = elgg_add_action_tokens_to_url($CONFIG->sslroot . 'action/googleappslogin/change_doc_permissions', FALSE);
 
+	// Set to globals
 	$GLOBALS['googleappslogin_url'] = $googleappslogin_url;
 	$GLOBALS['googleappsconnect_url'] = $googleappsconnect_url;
 	$GLOBALS['googleappsdisconnect_url'] = $googleappsdisconnect_url;
@@ -45,36 +48,72 @@ function googleappslogin_init() {
 	$GLOBALS['change_doc_permissions_url'] = $change_doc_permissions_url;
 	$GLOBALS['oauth_update_interval'] = get_plugin_setting('oauth_update_interval', 'googleappslogin');
 
+	// Get plugin settings
 	$oauth_sync_email = get_plugin_setting('oauth_sync_email', 'googleappslogin');
 	$oauth_sync_sites = get_plugin_setting('oauth_sync_sites', 'googleappslogin');
 	$oauth_sync_docs = get_plugin_setting('oauth_sync_docs', 'googleappslogin');
 
+	// Get google apps domain
 	$domain = get_plugin_setting('googleapps_domain', 'googleappslogin');
 	$GLOBALS['link_to_add_site'] = 'https://sites.google.com/a/' . $domain . '/sites/system/app/pages/meta/dashboard/create-new-site" target="_blank';
 
-	//$body = elgg_view("blogs/list", array('googleappslogin_url' => $googleappslogin_url));
-	//elgg_extend_view("account/forms/login", $body);
+	// Extend login view google login button
 	elgg_extend_view('login/extend', 'googleappslogin/login_dropdown');
+	
+	// @TODO: Do this somewhere else... (include oauth update scripts)
 	elgg_extend_view('messages/list', 'googleappslogin/scripts');
 
 	// Extend system CSS with our own styles
 	elgg_extend_view('css','googleappslogin/css');
+	
+	// Extend topbar view to add new mail icon
 	elgg_extend_view('elgg_topbar/extend','googleappslogin/new_mail');
 	
-	
+	// @TODO: What does this do.. its commented out (see function)
 	//register_plugin_hook('usersettings:save','user','googleappslogin_user_settings_save');
+	
+	// Register subtypes
 	register_entity_type('object','site_activity', 'Site activity');
-  register_entity_type('object','doc_activity', 'Doc activity');
-	$user = $_SESSION['user'];
+  	register_entity_type('object','doc_activity', 'Doc activity');
 
+	// Pagesetup event handler
 	register_elgg_event_handler('pagesetup','system','googleappslogin_pagesetup');
-	//register_elgg_event_handler('logout', 'user', 'googleappslogin_logout');
+	
+	// Login handler
 	register_elgg_event_handler('login', 'user', 'googleappslogin_login');
 
 	// TODO: remove this permissions hook if it turns out not to be necessary
 	register_plugin_hook('permissions_check','user','googleappslogin_can_edit');
 	register_plugin_hook('entity:icon:url','user','googleappslogin_icon_url');
 
+
+	//register CRON hook to poll for Google Site activity
+	register_plugin_hook('cron', 'fiveminute', 'googleapps_cron_fetch_data');
+
+	// Add menu items if user is synced and if sites/docs are enabled
+	$user = get_loggedin_user();
+	if (!empty($user) && $user->google) {
+		if ($oauth_sync_sites != 'no') {
+			// Sync wikis enabled
+			add_menu(elgg_echo('googleappslogin:sites'), $CONFIG->wwwroot . 'pg/wikis/' . $user->username);
+			register_page_handler('wikis','googleappslogin_page_handler');
+		}
+		if ($oauth_sync_docs != 'no') {
+			// Share docs enabled
+			register_page_handler('docs','googleappslogin_docs_page_handler');
+			add_menu(elgg_echo('googleappslogin:google_docs'), $CONFIG->wwwroot . 'pg/docs/my');
+		}
+	}
+	
+	// Set up page handler
+	register_page_handler('googleappsettings','googleappslogin_settings_page_handler');
+	
+	// Set up admin page handler
+	register_page_handler('googlesitesdebug','admin_googlesites_debug_page_handler');
+	
+	// Register widgets
+	add_widget_type('google_docs', elgg_echo('googleappslogin:google_docs'), elgg_echo('googleappslogin:google_docs:description'));
+	
 	// Register actions
 	register_action('googleappslogin/oauth_update', true, $CONFIG->pluginspath . 'googleappslogin/actions/oauth_update.php');
 	register_action('googleappslogin/login', true, $CONFIG->pluginspath . 'googleappslogin/actions/login.php');
@@ -87,52 +126,43 @@ function googleappslogin_init() {
 	register_action('googleappslogin/share_doc', false, $CONFIG->pluginspath . 'googleappslogin/actions/share_doc.php');
 	register_action('googleappslogin/change_doc_permissions', false, $CONFIG->pluginspath . 'googleappslogin/actions/change_doc_permissions.php');
 	register_action('googleappslogin/sites_reset',false, $CONFIG->pluginspath . 'googleappslogin/actions/sites_reset.php');
-
-	//register CRON hook to poll for Google Site activity
-	register_plugin_hook('cron', 'fiveminute', 'googleapps_cron_fetch_data');
-
-	if (!empty($user) && $user->google &&$oauth_sync_sites != 'no') {
-								//var_dump($oauth_sync_sites); die;
-                if  ($oauth_sync_sites != 'no') {
-                    add_menu(elgg_echo('googleappslogin:sites'), $CONFIG->wwwroot . 'pg/wikis/' . $_SESSION['user']->username);
-                    register_page_handler('wikis','googleappslogin_page_handler');
-                }
-
-                if ($oauth_sync_docs!= 'no') {
-                    register_page_handler('docs','googleappslogin_docs_page_handler');
-                    add_menu(elgg_echo('googleappslogin:google_docs'), $CONFIG->wwwroot . 'pg/docs/my');
-                }
-	}
-	register_page_handler('googleappsettings','googleappslogin_settings_page_handler');
-
-
-	//admin page stuff
-	//TODO: figure out how to order the submenu so that it appears at the bottom
-	register_page_handler('googlesitesdebug','admin_googlesites_debug');
-	elgg_add_submenu_item(array('text'=>elgg_echo('googleappslogin:admindebugtitle'),'href'=>"{$CONFIG->url}pg/googlesitesdebug",'id'=>'googlesitesdebug'),'admin');
-	
-
-	// Register widgets
-	add_widget_type('google_docs', elgg_echo('googleappslogin:google_docs'),
-	elgg_echo('googleappslogin:google_docs:description'));
 }
 
 function googleappslogin_pagesetup() {
 
 	global $CONFIG;
-	if (get_context() == "settings") {
-		add_submenu_item(elgg_echo('googleappslogin:google_sites_settings'), $CONFIG->wwwroot . "pg/googleappsettings/wikiactivity");
-		add_submenu_item(elgg_echo('googleappslogin:google_sync_settings'), $CONFIG->wwwroot . "pg/googleappsettings/account");
-	}
+	
+	$page_owner = elgg_get_page_owner();
 
-	if (get_context() == 'wikis') {
-		add_submenu_item(elgg_echo('googleappslogin:sites:your'), $CONFIG->wwwroot . 'pg/wikis/' . $_SESSION['user']->username);
-		add_submenu_item(elgg_echo('googleappslogin:sites:everyone'), $CONFIG->wwwroot . 'pg/wikis/all');
-		add_submenu_item(elgg_echo('googleappslogin:site:add'), $GLOBALS['link_to_add_site']);
-	}
+	// Settings items
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:google_sites_settings'), 
+								'href' => $CONFIG->wwwroot . "pg/googleappsettings/wikiactivity"), 'settings', 'z');
+
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:google_sync_settings'), 
+								'href' => $CONFIG->wwwroot . "pg/googleappsettings/account"), 'settings', 'z');	
+
+	// Wikis
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:sites:your'), 
+								'href' => $CONFIG->wwwroot . 'pg/wikis/' . $page_owner->username), 'wikis');
+
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:sites:everyone'), 
+								'href' => $CONFIG->wwwroot . 'pg/wikis/all'), 'wikis');
+														
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:site:add'), 
+								'href' => $GLOBALS['link_to_add_site']), 'wikis');
+	
+	// Admin
+	elgg_add_submenu_item(array('text' => elgg_echo('googleappslogin:admindebugtitle'),
+								'href'=> $CONFIG->url . "pg/googlesitesdebug",
+								'id'=>'googlesitesdebug'),'admin', 'zzz'); // zzz puts the debug at the bottom (alphabetically)
 }
 
-
+/**
+ * googleappslogin settings page handler
+ *
+ * @param array $page From the page_handler function
+ * @return true|false Depending on success
+ */
 function googleappslogin_settings_page_handler($page) {
 	gatekeeper();
 	global $SESSION;
@@ -143,74 +173,59 @@ function googleappslogin_settings_page_handler($page) {
 	
 	switch($page[0]) {
 		case 'wikiactivity':
-		
 			$form = elgg_view('googleappslogin/googlesites/form');
 			$body = elgg_view_layout('one_column_with_sidebar', $form);
 			page_draw(elgg_echo('googleappslogin:google_sites_settings'),$body);
-			
 		break;
 		case 'account':
-			
 			$body = elgg_view('googleappslogin/sync_form');
 			$body = elgg_view_layout('one_column_with_sidebar', $body);
 			page_draw(elgg_echo('googleappslogin:google_sync_settings'),$body);
-			
 		break;
 	}
-	
-	
 }
 
 
 /**
- * googleappslogin page handler; allows the use of fancy URLs
+ * googleappslogin docs page handler
  *
  * @param array $page From the page_handler function
  * @return true|false Depending on success
  */
 function googleappslogin_docs_page_handler($page) {
-    	if (isset($page[0])) {
+	if (isset($page[0])) {
 		switch ($page[0]) {
-
-                    case 'my':
-                        include(dirname(__FILE__) . '/docs.php');
-                        return true;
-
-                    case 'permissions':
-                        include(dirname(__FILE__) . '/docs_permissions.php');
-                        return true;
-                        
-                }
-        }
-        return true;
+       		case 'my':
+	        	include(dirname(__FILE__) . '/docs.php');
+	            return true;
+			break;
+            case 'permissions':
+            	include(dirname(__FILE__) . '/docs_permissions.php');
+                return true; 
+    		break;
+		}
+	}
+	return true;
 }
 
 /**
- * googleappslogin page handler; allows the use of fancy URLs
+ * googleappslogin page handler
  *
  * @param array $page From the page_handler function
  * @return true|false Depending on success
  */
 function googleappslogin_page_handler($page) {
-
-	// The second part dictates what we're doing
 	if (isset($page[0])) {
 		switch ($page[0]) {
-
 			case "all" :
-
 				$all = true;
 				include(dirname(__FILE__) . '/wikis.php');
 				return true;
-
-				break;
-
+			break;
 			default:
-
 				include(dirname(__FILE__) . '/wikis.php');
 				return true;
-
-				break;
+			break;
 		}
 	} else {
 		include(dirname(__FILE__) . '/wikis.php');
@@ -218,28 +233,60 @@ function googleappslogin_page_handler($page) {
 	}
 
 	return false;
-
 }
 
-function googleappslogin_logout() {
-//	setcookie('elgg_redirect', '0');
-//	setcookie('elgg_logout', mktime());
+/**
+ * Admin page handler
+ * @TODO: Not sure why need to many page handlers..
+ */
+function admin_googlesites_debug_page_handler($page) {
+	global $CONFIG;
+
+	admin_gatekeeper();
+	elgg_admin_add_plugin_settings_sidemenu();
+	set_context('admin');
+	
+	$content = elgg_view_title(elgg_echo("googleappslogin:admindebugtitle"));
+	$content .= elgg_view('googleappslogin/admin/sitesdebug_nav',array('page'=>$page));
+		
+	switch ($page[0]) {
+		case "byuser" :
+			$content .= list_googlesite_entities_byuser();
+		break;
+		case "reset":
+			$content .= elgg_view('googleappslogin/admin/reset');
+		break;
+		default:
+			$content .= list_googlesite_entities();
+		break;
+	}
+	
+	$body = elgg_view_layout('administration', array('content' => $content));
+	page_draw($title, $body, 'page_shells/admin');
 }
 
+/** 
+ * googleappslogin login event handler, triggered on login
+ */ 
 function googleappslogin_login() {
-
 	$oauth_sync_email = get_plugin_setting('oauth_sync_email', 'googleappslogin');
 	$oauth_sync_sites = get_plugin_setting('oauth_sync_sites', 'googleappslogin');
-        $oauth_sync_docs = get_plugin_setting('oauth_sync_docs', 'googleappslogin');
+	$oauth_sync_docs = get_plugin_setting('oauth_sync_docs', 'googleappslogin');
 
-	$user = $_SESSION['user'];
-	if (!empty($user) &&
-			$user->google &&
-			($oauth_sync_email != 'no' || $oauth_sync_sites != 'no' || $oauth_sync_docs != 'no')) {
-                                googleappslogin_get_oauth_data();
-                        }
+	$user = get_loggedin_user();
+	if (!empty($user) && 
+		$user->google &&
+		($oauth_sync_email != 'no' || $oauth_sync_sites != 'no' || $oauth_sync_docs != 'no')) 
+		{
+			// Email/docs/sites syncing enabled, so grab data
+			googleappslogin_get_oauth_data();
+		}
 }
 
+/** 
+ * Canedit plugin hook 
+ * @TODO: Might be a better way to allow access to googleapps data  
+ */
 function googleappslogin_can_edit($hook_name, $entity_type, $return_value, $parameters) {
 
 	$entity = $parameters['entity'];
@@ -249,13 +296,16 @@ function googleappslogin_can_edit($hook_name, $entity_type, $return_value, $para
 		return true;
 	}
 
-        if ($context == 'googleappslogin_cron_job') {
+	if ($context == 'googleappslogin_cron_job') {
 		return true;
 	}
 
 	return null;
 }
 
+/** 
+ * Icon plugin hook? 
+ */
 function googleappslogin_icon_url($hook_name,$entity_type, $return_value, $parameters) {
 
 	$entity = $parameters['entity'];
@@ -268,10 +318,9 @@ function googleappslogin_icon_url($hook_name,$entity_type, $return_value, $param
 	}
 }
 
+// @TODO: What does this do? Its not being called..
 function googleappslogin_user_settings_save() {
-
 	gatekeeper();
-
 	function update_acitivities_access($site_name, $access) {
 		$entities = get_entities_from_metadata('site_name', $site_name, 'object');
 		foreach ($entities as $entity) {
@@ -389,38 +438,4 @@ function googleappslogin_user_settings_save() {
 	}
 }
 
-function admin_googlesites_debug($page) {
-	global $CONFIG;
-
-	admin_gatekeeper();
-	elgg_admin_add_plugin_settings_sidemenu();
-	set_context('admin');
-	
-	$content = elgg_view_title(elgg_echo("googleappslogin:admindebugtitle"));
-	$content .= elgg_view('googleappslogin/admin/sitesdebug_nav',array('page'=>$page));
-		
-	switch ($page[0]) {
-		case "byuser" :
-			$content .= list_googlesite_entities_byuser();
-			break;
-		
-		case "reset":
-			$content .= elgg_view('googleappslogin/admin/reset');
-			break;
-		
-		default:
-			$content .= list_googlesite_entities();
-	}
-	
-	$body = elgg_view_layout('administration', $content);
-	page_draw($title, $body, 'page_shells/admin');
-}
-
-
-
 register_elgg_event_handler('init','system','googleappslogin_init');
-
-
-
-
-
