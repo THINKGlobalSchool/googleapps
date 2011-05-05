0603268f6e14b0d242d23a5449d4693201ed18d3
Jeff Tilson <jrtilson@gmail.com>
2011-05-05 22:33:32 +0000

    User name is now a link like it should be for wiki river entries

--- views/default/river/object/site_activity/create.php
+++ views/default/river/object/site_activity/create.php
@@ -10,10 +10,15 @@
 
 	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
 	$object = get_entity($vars['item']->object_guid);
-	$time = !empty($object) ? strtotime($object->updated) : 0;
-	$date = $time ? date('d M Y', $time) : '';
+
+
+	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	
 	$string = !empty($object->text) ? preg_replace("/\<div([^>]+)\>(.*?)\<\/div\>/", "$2", $object->text) : $object->text;
+	
+	// Make the owner name a link, like the rest of Elgg (this is nasty..)
+	$string = str_replace($performed_by->name, $url, substr($string, 0, strpos($string, '<'))) . substr($string, strpos($string, '<'));
+	
 	$string .= " <span class='entity_subtext'>" . friendly_time($object->time_created) . "</span>";
 ?>
 
